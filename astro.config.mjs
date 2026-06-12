a20626b866d2fa4e9858c235a0d65ab9f41ea051
JoshVC <josuevalenzuela87@gmail.com>
2026-06-12 13:29:49 -0400

    build: set dev server host, port and site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    site: 'http://ggggs0kkwoo4s0wckg4wcks0.72.62.165.86.sslip.io',
+  },
+});
